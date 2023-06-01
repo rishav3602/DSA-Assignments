ab6147d2137e2e2568e864235d54180bad00fdfe
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-06-01 14:43:29 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,192 @@
+"""
+<aside>
+💡 **Question 1**
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2),..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+**Example 1:**
+Input: nums = [1,4,3,2]
+Output: 4
+
+**Explanation:** All possible pairings (ignoring the ordering of elements) are:
+
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4
+</aside>"""
+
+def arrayPairSum(nums):
+    nums.sort()
+    pair_sum = 0
+    for i in range(0, len(nums), 2):
+        pair_sum += nums[i]
+    return pair_sum
+
+
+"""
+Question 2
+Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor. 
+
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice. 
+
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+
+Example 1:
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+
+Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type."""
+
+def distributeCandies(candyType):
+    unique_types = len(set(candyType))
+    max_types = len(candyType) // 2
+    return min(unique_types, max_types)
+
+"""
+Question 3
+We define a harmonious array as an array where the difference between its maximum value
+and its minimum value is exactly 1.
+
+Given an integer array nums, return the length of its longest harmonious subsequence
+among all its possible subsequences.
+
+A subsequence of an array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+Input: nums = [1,3,2,2,5,2,3,7]
+Output: 5
+
+Explanation: The longest harmonious subsequence is [3,2,2,2,3]."""
+
+def findLHS(nums):
+    count = {}
+    max_length = 0
+
+    # Count the frequency of each number
+    for num in nums:
+        count[num] = count.get(num, 0) + 1
+
+    # Check for harmonious subsequence length
+    for num in nums:
+        if num + 1 in count:
+            length = count[num] + count[num + 1]
+            max_length = max(max_length, length)
+
+    return max_length
+
+
+"""
+Question 4
+You have a long flowerbed in which some of the plots are planted, and some are not.
+However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true"""
+
+
+def canPlaceFlowers(flowerbed, n):
+    count = 0
+    length = len(flowerbed)
+    for i in range(length):
+        if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == length - 1 or flowerbed[i + 1] == 0):
+            count += 1
+            flowerbed[i] = 1
+    return count >= n
+
+
+"""
+Question 5
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: 6"""
+
+
+def maximumProduct(nums):
+    nums.sort()
+    return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
+
+
+"""
+Question 6
+Given an array of integers nums which is sorted in ascending order, and an integer target,
+write a function to search target in nums. If target exists, then return its index. Otherwise,
+return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+
+Explanation: 9 exists in nums and its index is 4"""
+
+def search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+"""
+Question 7
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is
+monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+Example 1:
+Input: nums = [1,2,2,3]
+Output: true
+"""
+
+def isMonotonic(nums):
+    increasing = decreasing = True
+
+    for i in range(1, len(nums)):
+        if nums[i] < nums[i - 1]:
+            increasing = False
+        if nums[i] > nums[i - 1]:
+            decreasing = False
+
+    return increasing or decreasing
+
+"""
+Question 8
+You are given an integer array nums and an integer k.
+
+In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+
+The score of nums is the difference between the maximum and minimum elements in nums.
+
+Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+
+Example 1:
+Input: nums = [1], k = 0
+Output: 0
+
+Explanation: The score is max(nums) - min(nums) = 1 - 1 = 0.
+"""
+
+def minScore(nums, k):
+    min_val = min(nums)
+    max_val = max(nums)
+
+    if min_val + k >= max_val - k:
+        return 0
+    else:
+        return max_val - k - (min_val + k)
+
